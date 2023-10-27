c9930e3c549441943d3c4351d1894adb8415cfe7
MardoX <wiktorprzylucki3@wp.pl>
2023-10-27 08:43:37 +0200

    ds

--- src/pl/gornik/Main.java
+++ src/pl/gornik/Main.java
@@ -1,12 +1,45 @@
 package pl.gornik;
+import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        BankAccount account = new BankAccount("12345678901234567",565.32);
+        BankAccount account = new BankAccount("12345678901234567",5000);
         System.out.println(account);
         System.out.println("Twoj numer konta: "+account.getNumberAccount());
-        account.setAccountBalance(account.getAccountBalance()-50000);
+        account.setAccountBalance(-5500);
         System.out.println(account.getAccountBalance());
+
+        account.deposit(500);
+        System.out.println("saldo po wplacie "+ account.getAccountBalance());
+        account.payCheck(1000);
+        System.out.println("saldo po wylacie "+ account.getAccountBalance());
+
+        int TimeDeposit = 1;
+        double amount = 2000;
+        double termPercent = 10;
+        LocalTime startTime = LocalTime.now();
+        LocalTime endTime = startTime.plusMinutes(TimeDeposit);
+        String end = "";
+        Scanner scanner = new Scanner(System.in);
+        while(!end.equals("stop")){
+            System.out.println("poczkeaj i potem wpisz: stop");
+            end = scanner.next();
+        }
+        LocalTime timeNow = LocalTime.now();
+        System.out.println();
+        System.out.println("przed lokatą: "+account.getAccountBalance());
+        TermDeposit termDeposit = new TermDeposit(TimeDeposit, amount, termPercent, startTime);
+        account.setAccountBalance(account.getAccountBalance() - amount);
+        if(timeNow.isBefore(endTime)){
+            System.out.println("Lokata trwa");
+        }
+        else {
+            account.setAccountBalance(
+                    account.getAccountBalance()
+                            + termDeposit.calculateProfit());
+            System.out.println("po lokacie: " + account.getAccountBalance());
+        }
     }
 }
---
+++ src/pl/gornik/TermDeposit.java
@@ -0,0 +1,21 @@
+package pl.gornik;
+
+import java.time.LocalTime;
+
+public class TermDeposit {
+    private int timeDeposit;
+    private double amount;
+    private double termPercent;
+    private LocalTime start;
+
+    public TermDeposit(int timeDeposit, double amount, double termPercent, LocalTime start) {
+        this.timeDeposit = timeDeposit;
+        this.amount = amount;
+        this.termPercent = termPercent;
+        this.start = start;
+    }
+
+    public double calculateProfit(){
+        return Math.round(amount *(1 + termPercent/100 * timeDeposit/12));
+    }
+}
